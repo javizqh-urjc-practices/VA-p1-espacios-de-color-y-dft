a381035b69fb51121e122c221b764bfaeba06a7e
SamthinkGit <sebastianmayorquin@gmail.com>
2024-02-16 09:32:31 +0100

    Exercises 0,1,2

--- src/computer_vision/CVSubscriber.cpp
+++ src/computer_vision/CVSubscriber.cpp
@@ -69,6 +69,48 @@ cv::Mat fftShift(const cv::Mat & magI)
   return magI_copy;
 }
 
+inline float rgb2hue(const float r, const float g, const float b){
+  return std::acos(
+    ( ((r-g) + (r-b))/2 )/
+    ( std::sqrt((std::pow(r-g,2) + (r-b)*(g-b))) )
+  );
+
+}
+
+namespace CVFunctions {
+
+cv::Mat BGR2HSI(const cv::Mat & image){
+
+  cv::Mat hsi_mat = cv::Mat::zeros(image.size(), image.type());
+  float r, g, b, h, s, i;
+
+  for (int u = 0; u < image.rows; u++) {
+    for (int v = 0; v < image.cols; v++) {
+
+      b = ((float)image.at<cv::Vec3b>(u,v)[0]) / 255;
+      g = ((float)image.at<cv::Vec3b>(u,v)[1]) / 255;
+      r = ((float)image.at<cv::Vec3b>(u,v)[2]) / 255;
+
+      h = rgb2hue(r,g,b);
+      s = 1 - 3 * std::min({r, g, b}) / (r + g + b);
+      i = (r+g+b)/3;
+
+
+      if (b > g) {
+        h = 360 - h;
+      }
+
+      hsi_mat.at<cv::Vec3b>(u,v)[0] = h*255/360;
+      hsi_mat.at<cv::Vec3b>(u,v)[1] = s*255;
+      hsi_mat.at<cv::Vec3b>(u,v)[2] = i*255;
+    }
+  }
+
+  return hsi_mat;
+
+}
+
+}
 
 // Calculate dft spectrum
 cv::Mat spectrum(const cv::Mat & complexI)
@@ -94,6 +136,14 @@ cv::Mat spectrum(const cv::Mat & complexI)
                                                       // viewable image form (float between values 0 and 1).
   return spectrum;
 }
+
+namespace CVParams {
+
+  inline bool running = false;
+  inline std::string WINDOW_NAME = "Practica_5";
+
+}
+
 namespace computer_vision
 {
 
@@ -107,7 +157,7 @@ CVGroup CVSubscriber::processing(
 const
 {
   // Create output images
-  cv::Mat out_image_rgb, out_image_depth;
+  cv::Mat out_image_rgb, out_image_depth, preprocessed_image, hsv_mat;
   // Create output pointcloud
   pcl::PointCloud<pcl::PointXYZRGB> out_pointcloud;
 
@@ -116,10 +166,35 @@ const
   out_image_depth = in_image_depth;
   out_pointcloud = in_pointcloud;
 
-  // Show images in a different windows
-  cv::imshow("out_image_rgb", out_image_rgb);
-  cv::imshow("out_image_depth", out_image_depth);
-  cv::waitKey(3);
+  // First time execution
+  if (!CVParams::running) {
+
+    CVParams::running = true;
+
+    cv::namedWindow(CVParams::WINDOW_NAME);
+
+    // create Trackbar and add to a window
+    cv::createTrackbar("mode", CVParams::WINDOW_NAME, nullptr, 7, 0);
+  }
+    
+
+  switch (cv::getTrackbarPos("mode", CVParams::WINDOW_NAME))
+  {
+  case 1:
+    preprocessed_image = CVFunctions::BGR2HSI(in_image_rgb);
+    break;
+  case 2:
+    cv::cvtColor(out_image_rgb, hsv_mat, cv::COLOR_BGR2HSV);
+    preprocessed_image = hsv_mat - CVFunctions::BGR2HSI(in_image_rgb);
+    break;
+
+  default:
+    preprocessed_image = out_image_rgb;
+    break;
+  }
+
+  cv::imshow(CVParams::WINDOW_NAME, preprocessed_image);
+  cv::waitKey(1);
 
   return CVGroup(out_image_rgb, out_image_depth, out_pointcloud);
 }
